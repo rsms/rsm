4a4bbeb2adba3b0712a1e2d2ab1ea4ad953b2d5a
Rasmus Andersson <rasmus@notion.se>
2022-02-16 00:00:14 -0800

    simplify build script

--- build.sh
+++ build.sh
@@ -6,12 +6,14 @@ _err() { echo -e "$0:" "$@" >&2 ; exit 1; }
 OUTDIR=out
 BUILD_MODE=safe  # debug | safe | fast
 WATCH=
+_WATCHED=
 RUN=
 NINJA_ARGS=()
 TESTING_ENABLED=
 
 while [[ $# -gt 0 ]]; do case "$1" in
   -w)      WATCH=1; shift ;;
+  -_w_)    _WATCHED=1; shift ;;
   -v)      NINJA_ARGS+=(-v); shift ;;
   -run=*)  RUN=${1:5}; shift ;;
   -debug)  BUILD_MODE=debug; TESTING_ENABLED=1; shift ;;
@@ -59,7 +61,7 @@ if [ -n "$WATCH" ]; then
   while true; do
     printf "\x1bc"  # clear screen ("scroll to top" style)
     BUILD_OK=1
-    ${SHELL:-bash} "./$(basename "$0")" "-$BUILD_MODE" "${NINJA_ARGS[@]}" "$@" || BUILD_OK=
+    ${SHELL:-bash} "./$(basename "$0")" -_w_ "-$BUILD_MODE" "${NINJA_ARGS[@]}" "$@" || BUILD_OK=
     printf "\e[2m> watching files for changes...\e[m\n"
     if [ -n "$BUILD_OK" -a -n "$RUN" ]; then
       export ASAN_OPTIONS=detect_stack_use_after_return=1
@@ -85,25 +87,28 @@ if [ -n "$WATCH" ]; then
   exit 0
 fi
 
-
-# use llvm at from homebrew if available
-if [ -x /usr/local/opt/llvm/bin/clang ]; then
-  export PATH=/usr/local/opt/llvm/bin:$PATH
-  export CC=clang
-  export CXX=clang++
-  export AR=llvm-ar
-  CC_IS_CLANG=1
-else
-  export CC=${CC:-cc}
+if [ -z "$CC" ]; then
+  # use clang from known preferred location, if available
+  if [ -x /usr/local/opt/llvm/bin/clang ]; then
+    export PATH=/usr/local/opt/llvm/bin:$PATH
+    export CC=clang
+    CC_IS_CLANG=1
+  elif [ -x /opt/homebrew/opt/llvm/bin/clang ]; then
+    export PATH=/opt/homebrew/opt/llvm/bin:$PATH
+    export CC=clang
+    CC_IS_CLANG=1
+  else
+    export CC=cc
+  fi
 fi
-CC_PATH=$(command -v $CC)
-CC_PATH=${CC_PATH##$PWD/}
+CC_PATH=$(command -v "$CC" || true)
 [ -f "$CC_PATH" ] || _err "CC (\"$CC\") not found"
 if [ -z "$CC_IS_CLANG" ] && $CC --version 2>/dev/null | head -n1 | grep -q clang; then
   CC_IS_CLANG=1
 fi
+[ -z "$_WATCHED" ] && echo "using compiler $CC_PATH"
 
-# check compiler and clear $OUTDIR if cflags or compiler changed
+# check compiler and clear $OUTDIR if compiler changed
 CCONFIG_FILE=$OUTDIR/cconfig.txt
 CCONFIG="$CC_PATH: $(sha256sum "$CC_PATH" | cut -d' ' -f1)"
 if [ "$(cat "$CCONFIG_FILE" 2>/dev/null)" != "$CCONFIG" ]; then
@@ -112,7 +117,6 @@ if [ "$(cat "$CCONFIG_FILE" 2>/dev/null)" != "$CCONFIG" ]; then
   mkdir -p "$OUTDIR"
   echo "$CCONFIG" > "$CCONFIG_FILE"
 fi
-
 
 CFLAGS=( $CFLAGS )
 LDFLAGS=( $LDFLAGS )
@@ -128,7 +132,6 @@ else
   CFLAGS+=( -DDEBUG -ferror-limit=6 )
   [ -n "$TESTING_ENABLED" ] && CFLAGS+=( -DR_TESTING_ENABLED )
 fi
-
 
 # enable llvm address and UD sanitizer in debug builds
 if [ -n "$CC_IS_CLANG" -a "$BUILD_MODE" = "debug" ]; then
@@ -189,31 +192,17 @@ rule cc
   depfile = \$out.d
   description = compile \$in
 
-rule cxx
-  command = $CXX -MMD -MF \$out.d \$cflags \$cflags_cxx -c \$in -o \$out
-  depfile = \$out.d
-  description = compile \$in
-
 _END
 
 
 _objfile() { echo \$objdir/${1//\//.}.o; }
 _gen_obj_build_rules() {
-  local CFLAGS=$1 ; shift
-  local CXXFLAGS=$1 ; shift
   local OBJECT
   for SOURCE in "$@"; do
     OBJECT=$(_objfile "$SOURCE")
     case "$SOURCE" in
       *.c|*.m)
         echo "build $OBJECT: cc $SOURCE" >> build.ninja
-        [ -n "$CFLAGS" ] &&
-          echo "  cflags = \$cflags $CFLAGS" >> build.ninja
-        ;;
-      *.cc|*.cpp|*.mm)
-        echo "build $OBJECT: cxx $SOURCE" >> build.ninja
-        [ -n "$CXXFLAGS" ] &&
-          echo "  cflags = \$cflags $CXXFLAGS" >> build.ninja
         ;;
       *) _err "don't know how to compile this file type ($SOURCE)"
     esac
@@ -227,7 +216,7 @@ else
   R_SOURCES=( $(find src -name '*.c' -not -name '*_test.c' -not -name 'test.c') )
 fi
 
-R_OBJECTS=( $(_gen_obj_build_rules "" "" "${R_SOURCES[@]}") )
+R_OBJECTS=( $(_gen_obj_build_rules "${R_SOURCES[@]}") )
 echo >> build.ninja
 
 echo "build rsm: phony \$builddir/rsm" >> build.ninja

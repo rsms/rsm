b1b363ef125453b4c76d533f71fa7232ebb0f772
Rasmus Andersson <rasmus@notion.se>
2022-02-21 12:34:12 -0800

    define __has_* macros for compilers without them

--- src/rsmimpl.h
+++ src/rsmimpl.h
@@ -43,6 +43,16 @@
   #define static_assert _Static_assert
 #endif
 
+#ifndef __has_extension
+  #define __has_extension   __has_feature
+#endif
+#ifndef __has_include
+  #define __has_include(x)  0
+#endif
+#ifndef __has_builtin
+  #define __has_builtin(x)  0
+#endif
+
 #if __has_attribute(fallthrough)
   #define FALLTHROUGH __attribute__((fallthrough))
 #else

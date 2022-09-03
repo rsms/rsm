9a965286c9f1905fb46bc60d1719bd38d84b38ff
Rasmus Andersson <rasmus@notion.se>
2022-09-02 20:23:01 -0700

    fix test.sh script after changes to build products in build.sh

--- test.sh
+++ test.sh
@@ -11,12 +11,12 @@ for f in examples/*.rsm; do
     fail-*) continue ;;  # skip files matching "fail-*.rsm"
   esac
 
-  ROM=out/test_${FILENAME%*.rsm}.rom
+  ROM=out/safe/test_${FILENAME%*.rsm}.rom
 
   echo "rsm -o '$ROM' '$f'"
-  out/rsm -o "$ROM" "$f"
+  out/safe/rsm -o "$ROM" "$f"
 
   # Note: null stdin to avoid read on stdin blocking the test script
   echo "rsm -R0=3 '$ROM'"
-  out/rsm -R0=3 "$ROM" </dev/null
+  out/safe/rsm -R0=3 "$ROM" </dev/null
 done

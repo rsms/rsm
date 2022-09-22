76ddcd5ef270f7eeb7ac502b17d6e023ce745e6d
Rasmus Andersson <rasmus@notion.se>
2022-09-22 09:45:19 -0700

    add example of stack growth

--- test.sh
+++ test.sh
@@ -4,17 +4,22 @@ cd "$(dirname "$0")"
 
 ./build.sh "$@"
 
-for f in examples/*.rsm; do
+for srcfile in examples/*.rsm; do
   echo "——————————————————————————————————————————————————————————————————————————"
-  FILENAME=${f##*/}
+  FILENAME=${srcfile##*/}
+
+  # skip files matching "fail-*.rsm"
   case "$FILENAME" in
-    fail-*) continue ;;  # skip files matching "fail-*.rsm"
+    fail-*) continue ;;
   esac
+
+  # skip source files containing "//!exe2-only"
+  grep -qE "^\/\/\!exe2-only" "$srcfile" && continue
 
   ROM=out/safe/test_${FILENAME%*.rsm}.rom
 
-  echo "rsm -o '$ROM' '$f'"
-  out/safe/rsm -o "$ROM" "$f"
+  echo "rsm -o '$ROM' '$srcfile'"
+  out/safe/rsm -o "$ROM" "$srcfile"
 
   # Note: null stdin to avoid read on stdin blocking the test script
   echo "rsm -R0=3 '$ROM'"
